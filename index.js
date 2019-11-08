8313e84fef902a60f19bf3bfdd3251eea84fdbe4
junblee <topguide82@gmail.com>
2019-11-08 14:04:21 +0900

    function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-app.get('/', function(req,res){
-    console.log(`Listenning port : ${PORT}`)
-    res.send("Hello World Again")
-});
-app.listen(PORT);
+
+function Listening(){
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+function Home(req,res){
+    console.log("Homepage");
+    res.send("Hello World Again Again Again!!");
+};
+app.get('/',Home);
+app.listen(PORT,Listening);
