1076823dfae9a5c02e89b6d80f9aaf5c4b614bdd
junblee <topguide82@gmail.com>
2019-11-11 23:30:11 +0900

    middlewares

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -16,6 +17,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("dev"));
+
+app.use(localMiddleware)
 
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter);
