189f76b72ab41e240bd14d075aeee6ab75f0c157
junblee <topguide82@gmail.com>
2019-11-12 16:18:38 +0900

    nodemon

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express = require('express');
+import express from "express"
+const app = express();
+const PORT = 4000;
+
+const listen = () => console.log(`listening port : ${PORT}`);
+const handle = (req,res) => res.send('Profile Page');
+const funcGet = (req,res) => res.send('Wellllllll??');
+
+app.get('/',funcGet);
+app.get('/profile',handle);
+
+app.listen(PORT,listen);
