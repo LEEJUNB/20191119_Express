993250eb01695eb53f72d24e3829ff7f6c0451eb
junblee <topguide82@gmail.com>
2019-11-12 15:36:24 +0900

    re install

--- init.js
+++
@@ -1,5 +0,0 @@
-import app from './app';
-
-const PORT = 3000;
-const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
-app.listen(PORT,handleListening);
--- middlewares.js
+++
@@ -1,7 +0,0 @@
-import routes from "./routes";
-
-export const localMiddleware = (req,res,next) => {
-    res.locals.siteName = "Juntube!!";
-    res.locals.routes = routes;
-    next();
-};
