72a8590eb1f319d88bc82a139f3361160f196d37
junblee <topguide82@gmail.com>
2019-11-12 17:42:59 +0900

    init

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
 
 const app = express();
-const PORT = 4000;
 
-const listen = () => console.log(`listening port : ${PORT}`);
 const handle = (req,res) => res.send('Profile Page');
 const funcGet = (req,res) => res.send('Wellllllll??');
 
@@ -20,4 +18,4 @@ app.use(cookieParser());
 app.get('/',funcGet);
 app.get('/profile',handle);
 
-app.listen(PORT,listen);
+export default app;
---
+++ init.js
@@ -0,0 +1,8 @@
+import app from './app'
+
+const PORT = 4000;
+
+const listen = () => console.log(`listening port : ${PORT}`);
+
+app.listen(PORT,listen);
+
