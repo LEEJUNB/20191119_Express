2a0acbc6d6a18d58815f3863609d5bac58b20739
junblee <topguide82@gmail.com>
2019-11-13 23:52:22 +0900

    import routers

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import morgan from "morgan"
 import helmet from "helmet"
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter"
+import globalRouter from "./routers/globalRouter"
 
 const app = express();
 
@@ -11,5 +14,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+
+app.use("/",globalRouter);
+app.use("/user",userRouter);
+app.use("/video",videoRouter);
 
 export default app;
